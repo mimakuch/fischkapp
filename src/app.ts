4060f8b2a2cd86b68bcbb4505437d33571cc0282
Michal Makuch <michalmakuch@hotmail.com>
2023-05-15 00:19:34 +0200

    feat: ts-for-addCard-added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { createState } from "./data/createState.js";
 // import { createCardComponent } from "./ui/card.js";
 // import { addCard } from "./data/actions.js";
 import {createHeaderComponent} from "./ui/header.js";
-import {createAddCardComponent} from "./ui/addCard";
+import {createAddCardComponent} from "./ui/addCard.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Example of how we can create app state responsible for holding data
@@ -20,6 +20,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const newCard = createAddCardComponent({
     newCardClass: 'card',
+    frontSideClass: 'card__front',
+    inputClass: 'card__input',
+    lineClass: 'card__line',
+    wrapperClass: 'card__buttons',
+    backSideClass: 'card__back',
+    deleteWrapperClass: 'card__delete',
+    inputValueClass: 'card__delete--input',
+    deleteIconClass: 'card__delete--icon',
   });
 
   const app = document.getElementById("app") as HTMLDivElement;
--- src/ui/addCard.ts
+++ src/ui/addCard.ts
@@ -1,21 +1,82 @@
+import {createButtonComponent} from "./button";
+
 interface AddCardParams {
     newCardClass: string;
+    frontSideClass: string;
+    inputClass: string;
+    lineClass: string;
+    wrapperClass: string;
+    backSideClass: string;
+    deleteWrapperClass: string;
+    inputValueClass: string;
+    deleteIconClass: string;
 }
 
 export const createAddCardComponent = (params: AddCardParams): {render: () => HTMLElement} => {
 
     const newCard = document.createElement('div');
+
+    // FRONT SIDE OF THE CARD
     const frontSide = document.createElement('div');
-    const input = document.createElement('input');
-    const horizontalLine = document.createElement('hr');
-    const cancelButton = document.createElement('button');
-    const nextButton = document.createElement('button');
+    const frontInput = document.createElement('input');
+    const frontHorizontalLine = document.createElement('hr');
+    const frontButtonsWrapper = document.createElement('div');
+    const cancelButton = createButtonComponent({
+        buttonText: 'cancel',
+        buttonClass: 'card__buttons--cancel',
+    });
+    const nextButton = createButtonComponent({
+        buttonText: 'next',
+        buttonClass: 'card__buttons--next',
+    });
+
+    // BACK SIDE OF THE CARD
     const backSide = document.createElement('div');
+    const deleteWrapper = document.createElement('div');
+    const inputValue = document.createElement('span');
+    const deleteIcon = document.createElement('img');
+    const backInput = document.createElement('input');
+    const backHorizontalLine = document.createElement('hr');
+    const backButtonsWrapper = document.createElement('div');
+    const backButton = createButtonComponent({
+        buttonText: 'back',
+        buttonClass: 'card__buttons--back',
+    });
+    const saveButton = createButtonComponent({
+        buttonText: 'save',
+        buttonClass: 'card__buttons--save',
+    });
 
-
-
+    // LIST OF CLASSES
     newCard.classList.add(params.newCardClass);
+    frontSide.classList.add(params.frontSideClass);
+    frontInput.classList.add(params.inputClass);
+    frontHorizontalLine.classList.add(params.lineClass);
+    frontButtonsWrapper.classList.add(params.wrapperClass);
+    backSide.classList.add(params.backSideClass);
+    deleteWrapper.classList.add(params.deleteWrapperClass);
+    inputValue.classList.add(params.inputValueClass);
+    deleteIcon.classList.add(params.deleteIconClass);
+    backInput.classList.add(params.inputClass);
+    backHorizontalLine.classList.add(params.lineClass);
+    backButtonsWrapper.classList.add(params.wrapperClass);
 
+    // CONNECTING PARTS OF THE COMPONENT
+    newCard.appendChild(frontSide);
+    newCard.appendChild(backSide);
+    frontSide.appendChild(frontInput);
+    frontSide.appendChild(frontHorizontalLine);
+    frontSide.appendChild(frontButtonsWrapper);
+    frontButtonsWrapper.appendChild(cancelButton.render());
+    frontButtonsWrapper.appendChild(nextButton.render());
+    backSide.appendChild(deleteWrapper);
+    deleteWrapper.appendChild(inputValue);
+    deleteWrapper.appendChild(deleteIcon);
+    backSide.appendChild(backInput);
+    backSide.appendChild(backHorizontalLine);
+    backSide.appendChild(backButtonsWrapper);
+    backButtonsWrapper.appendChild(backButton.render());
+    backButtonsWrapper.appendChild(saveButton.render());
 
     const render = () => {
         return newCard;
