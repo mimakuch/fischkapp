24a73276b935ba671efb723cebaa859796633a19
Michal Makuch <michalmakuch@hotmail.com>
2023-05-18 15:05:49 +0200

    feat: card-component-ui

--- src/ui/card.ts
+++ src/ui/card.ts
@@ -1,17 +1,33 @@
-// export const createCardComponent = (params) => {
-//   let state = {
-//     isFront: true,
-//   };
-//
-//   const handleToggleCard = () => {
-//     state = { ...state, isFront: !state.isFront };
-//     card.innerText = state.isFront ? params.front : params.back;
-//   };
-//
-//   const card = document.createElement("div");
-//   card.classList.add("card");
-//   card.addEventListener("click", handleToggleCard);
-//   card.innerText = params.front;
-//
-//   return card;
-// };
+interface CardParams {
+  front: string;
+  back: string;
+  iconPath: string;
+  editButtonClass: string;
+}
+
+export const createCardComponent = (params: CardParams) => {
+  let state = {
+    isFront: true,
+  };
+
+  const handleToggleCard = (): void => {
+    state = { ...state, isFront: !state.isFront };
+    card.innerText = state.isFront ? params.front : params.back;
+    card.classList.toggle('flipped');
+  };
+
+  const card = document.createElement('div');
+  const button = document.createElement('button');
+  const editIcon = document.createElement('img');
+
+  card.classList.add('card');
+  card.addEventListener('click', handleToggleCard);
+  card.innerText = params.front;
+
+  editIcon.src = params.iconPath;
+  button.classList.add(params.editButtonClass);
+  button.appendChild(editIcon);
+  card.appendChild(button);
+
+  return card;
+};
